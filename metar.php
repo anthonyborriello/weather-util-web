7c046cbaa8408d04600e3d669ab4f11c01d1f57e
Antonio Borriello <57049017+anthonyborriello@users.noreply.github.com>
2023-12-20 18:13:38 +0100

    Update metar.php

--- metar.php
+++ metar.php
@@ -15,12 +15,12 @@
 <style>
 body {
   padding-top: 20px;
+  background-color: #f4f4f4;
 }
 h1 {
   font-family: monospace;
   text-align: center;
   padding-top: 12px;
-  padding-bottom: 4px;
 }
 form {
   text-align: center;
@@ -33,11 +33,12 @@ form {
   padding-top: 20px;
 }
 .content {
+  background-color: white;
   max-width: max-content;
   margin: 0 auto;
   padding-left: 45px;
   padding-right: 45px;
-  padding-bottom: 45px; 
+  padding-bottom: 40px; /* Padding should be 45 but 42 is better :) */
   box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
 }
 pre {
@@ -53,30 +54,32 @@ pre {
   padding-left: 20px;
   padding-right: 20px;
   box-shadow: none;
+  padding-bottom: 30px;
  }
 pre {
   word-wrap: break-word;
   overflow-x: auto;
   overflow-y: hidden;
   margin-bottom: 0;
-  padding-bottom: 30px; 
  }
 }
 </style>
 </head>
 <body>
-<div class="content">
-<form id="metarForm" method="post" action="">
-  <h1>METAR</h1>
-  <label for="aeroporto">Select airport:</label>
-  <select id="aeroporto" name="aeroporto">
-    <option value="lirn" <?= $_POST['aeroporto'] == 'lirn' ? 'selected' : '' ?>>LIRN (Napoli)</option>
-    <option value="liri" <?= $_POST['aeroporto'] == 'liri' ? 'selected' : '' ?>>LIRI (Salerno)</option>
-    <option value="lirm" <?= $_POST['aeroporto'] == 'lirm' ? 'selected' : '' ?>>LIRM (Grazzanise)</option>
-    <option value="liqc" <?= $_POST['aeroporto'] == 'liqc' ? 'selected' : '' ?>>LIQC (Capri)</option>
-  </select>
-  <input type="submit" value="Send" id="submitBtn">
-</form>
+  <div class="content">
+    <div class="header">
+      <h1>METAR</h1>
+    </div>
+    <form id="metarForm" method="post" action="">
+        <label for="aeroporto">Select airport:</label>
+        <select id="aeroporto" name="aeroporto">
+            <option value="lirn" <?= $_POST['aeroporto'] == 'lirn' ? 'selected' : '' ?>>LIRN (Napoli)</option>
+            <option value="liri" <?= $_POST['aeroporto'] == 'liri' ? 'selected' : '' ?>>LIRI (Salerno)</option>
+            <option value="lirm" <?= $_POST['aeroporto'] == 'lirm' ? 'selected' : '' ?>>LIRM (Grazzanise)</option>
+            <option value="liqc" <?= $_POST['aeroporto'] == 'liqc' ? 'selected' : '' ?>>LIQC (Capri)</option>
+        </select>
+        <input type="submit" value="Send" id="submitBtn">
+    </form>
 
 <p id="caricamento" style="display: none;">Caricamento in corso...</p>
 

adbced5c954578d74358872ce8261b748dff1deb
Antonio Borriello <57049017+anthonyborriello@users.noreply.github.com>
2025-12-02 23:01:14 +0100

    Update metar.php

--- metar.php
+++ metar.php
@@ -1,17 +1,3 @@
-<!--
-weather-util web interface
-Author: Antonio Borriello
-GitHub: https://github.com/anthonyborriello/weather-util-web
-License: MIT License
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-and associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED.
--->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -104,3 +90,17 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
   </div>
 </body>
 </html>
+
+<!--
+weather-util web interface
+Author: Antonio Borriello
+GitHub: https://github.com/anthonyborriello/weather-util-web
+License: MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED.
+-->
